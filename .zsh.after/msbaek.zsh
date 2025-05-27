578a43ac73826f93e61a25640ed738fc7c6a3846
Myeongseok Baek <codetemplate@hanmail.net>
2025-05-27 11:16:24 +0900

    change msaek.zsh .gitconfig

--- .zsh.after/msbaek.zsh
+++ .zsh.after/msbaek.zsh
@@ -57,7 +57,6 @@ alias ta='tmux attach'
 set -o vi
 
 alias vi='nvim'
-alias gs='git status'
 alias gl='git log'
 alias find_wifi_pwd='security find-generic-password -wa '
 
@@ -74,7 +73,6 @@ export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init --path)"
 eval "$(pyenv init -)"
 
-export TT="zzz"
 export PATH="$HOME/.local/bin:$PATH"
 
 export OH_MY_ZSH=/users/msbaek/.oh-my-zsh/
@@ -202,5 +200,8 @@ export PATH=$GOPATH/bin:$GOROOT/bin:$HOME/.local/bin:$PATH
 alias gdum='gdu -h -d 1'
 alias d2h='diff2html -s side'
 unalias gsd
+
+export AWS_PROFILE=default
+export ANTHROPIC_MODEL='us.anthropic.claude-sonnet-4-20250514-v1:0'
 
 FUNCNEST=100
